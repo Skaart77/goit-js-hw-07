64c493af5e40ff2e63f12ad38e22fda142ee730b
Serhii Skliarov <114434801+Skaart77@users.noreply.github.com>
2023-03-02 11:30:13 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,9 @@ const picturesContainer = document.querySelector('.gallery');
 const createGalleryMarkup = galleryItems => {
     return galleryItems.map(({ original, preview, description })=> {
         return `
-        <a class="gallery__item" href="${original}">
+       <li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src=${preview} alt="${description}" />
-</a>`;
+</a></li>`;
     }).join('');
 };
 
